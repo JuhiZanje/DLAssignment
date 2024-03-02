51247548984fff37bc1cd501eb5e0b24d9dfd1bb
unknown <juhizanje@gmail.com>
2024-03-02 10:02:55 +0530

    code for batch and mini-batch gradient descent as well as testing added

--- Assignment1.py
+++ Assignment1.py
@@ -25,6 +25,9 @@ class MultiLayerPerceptron():
     def relu(self, x):
         return np.maximum(0, x)
 
+    def relu_d(self, x):
+        return (x > 0) * 1
+    
     def softmax(self, x):
         exps = np.exp(x - np.max(x, axis=1, keepdims=True))
         return exps / np.sum(exps, axis=1, keepdims=True)
@@ -59,22 +62,83 @@ class MultiLayerPerceptron():
             if i > 0:
                 dL_dA = np.dot(dL_dA * dA_dZ, MultiLayerPerceptron.parameters[f'w_{i+1}'].T)
 
+    def batch_gradient_descent(self, epochs=1000, batch_size=32, learning_rate=0.01):
+        batch_size=self.no_of_samples
+        no_of_batches = self.no_of_samples // batch_size
 
+        for i in range(epochs):
+            for j in range(no_of_batches):
+                start = j * batch_size
+                end = (j + 1) * batch_size
+                Y_train = self.train_labels[start:end]
+                X_train = self.train_data[start:end]
+
+                self.feed_forwards(X_train)
+                self.back_prop(X_train, Y_train)
+
+                for k in range(len(self.layer_sizes) - 1):
+                    MultiLayerPerceptron.parameters[f'w_{k+1}'] -= learning_rate * (MultiLayerPerceptron.gradients[f'w_{k+1}'] / batch_size)
+                    MultiLayerPerceptron.parameters[f'b_{k+1}'] -= learning_rate * (MultiLayerPerceptron.gradients[f'b_{k+1}'] / batch_size)                
+
+
+            if i == 0 or (i + 1) % 10 == 0:
+                loss = self.cross_entropy_loss(self.layer_outputs[-1], Y_train)
+                print(f'EPOCH NO: {i + 1} loss:{loss}')
+
+    def train(self, epochs=50, batch_size=32, learning_rate=0.01):
+        #this is mini batch gradient descent 
+        #use this to train as this converges faster and gives better accuracy
+        no_of_batches = self.no_of_samples // batch_size
+
+        for i in range(epochs):
+            for j in range(no_of_batches):
+                start = j * batch_size
+                end = (j + 1) * batch_size
+                Y_train = self.train_labels[start:end]
+                X_train = self.train_data[start:end]
+
+                self.feed_forwards(X_train)
+                self.back_prop(X_train, Y_train)
+
+                for k in range(len(self.layer_sizes) - 1):
+                    MultiLayerPerceptron.parameters[f'w_{k+1}'] -= learning_rate * (MultiLayerPerceptron.gradients[f'w_{k+1}'] / batch_size)
+                    MultiLayerPerceptron.parameters[f'b_{k+1}'] -= learning_rate * (MultiLayerPerceptron.gradients[f'b_{k+1}'] / batch_size)
+
+            if i == 0 or (i + 1) % 10 == 0:
+                print(f'EPOCH NO: {i + 1}')
+
+    def test(self, x, y):
+        self.feed_forwards(x)
+        loss = self.cross_entropy_loss(self.layer_outputs[-1], y)
+        acc = self.accuracy(self.layer_outputs[-1], y)
+        print(f'TEST LOSS: {loss:.4f} ACCURACY: {acc*100:.4f}')
+
+    def cross_entropy_loss(self, pred, actual):
+        return -np.sum(actual * np.log(pred + 1e-9)) / len(pred)
+    
+    def accuracy(self, pred, truth):
+        return (np.argmax(truth, axis=1) == np.argmax(pred, axis=1)).mean()
+
+#Load dataset
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 total_class=max(train_labels)
 
+train_images = train_images.reshape(train_images.shape[0], -1) / 255.0
+test_images = test_images.reshape(test_images.shape[0], -1) / 255.0
+
 #change trainlabel=2 to oneHot=[0 0 1 0 0 0 0 0 0 0]
 train_labels_one_hot = np.eye(10)[train_labels]
-# test_labels_one_hot = np.eye(10)[test_labels]
+test_labels_one_hot = np.eye(10)[test_labels]
 
 # print(train_labels.ndim)
 # y_train_one_hot = np.eye(10)[train_labels]
 # print(train_labels[0])
 # print(y_train_one_hot[0])
 
+#We can give any number of layers and their sizes here
+layer_sizes = [train_images.shape[1],128, 128,128,10]  # Input, hidden1, hidden2,hidden3 output
 
-layer_sizes = [train_images.shape[1],128, 128,128,total_class]  # Input, hidden1, hidden2,hidden3 output
-
+#init , train and test our model
 mlp = MultiLayerPerceptron(train_images, train_labels_one_hot, layer_sizes)
-mlp.feed_forwards(train_images)
-mlp.back_prop(train_images,train_labels_one_hot)
+mlp.train()
+mlp.test(test_images, test_labels_one_hot)
